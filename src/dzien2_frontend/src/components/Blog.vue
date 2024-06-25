0bb316b3d58485515324241092f10d7f2db33d92
CepusMaximus1910 <alex.koledzy@onet.pl>
2024-06-25 19:56:38 +0200

    Added a way to add new wpis

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -3,22 +3,29 @@
         <button @click="pobierzWpisy">refresh</button>
         siema blog!
         {{ wpisy }}
+        <input v-model="nowyBlog" type="text">
+        <button @click="dodajWpisy">dodaj</button>
     </div>
 </template>
 
 <script>
-import { dzien2_backend } from "declarations/dzien2_backend/index";
+import { dzien2_backend } from 'declarations/dzien2_backend/index';
 
 export default {
     data() {
         return {
-            wpisy: []
+            wpisy: [],
+            nowyBlog: ""
         }
     },
     methods: {
+        async dodajWpisy() {
+            await dzien2_backend.dodaj_wpis(this.nowyBlog);
+        },
         async pobierzWpisy() {
-            await dzien2_backend.odczytaj_wpisy();
+            this.wpisy = await dzien2_backend.odczytaj_wpisy();
         }
-    }
+    },
+
 }
 </script>
